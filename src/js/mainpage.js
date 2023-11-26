eba1fc9de679f9ed2dc06fc647c29618f9b0e7ab
Alessandro Martini <alessandro.martini10@studio.unibo.it>
2023-11-26 16:50:31 +0100

    modify main page all, add footer and icon

--- src/js/mainpage.js
+++ src/js/mainpage.js
@@ -1,63 +1,60 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const postsContainer = document.getElementById('posts-container');
-  
-    posts.forEach(post => {
-      const postMarkup = createPostMarkup(post);
-      postsContainer.innerHTML += postMarkup;
-    });
-  
-    postsContainer.addEventListener('click', function (event) {
+  const postsContainer = document.getElementById('posts-container');
+
+  renderPosts();
+
+  postsContainer.addEventListener('click', function (event) {
       const likeButton = event.target.closest('.like-button');
-  
-      if (likeButton) {
-        likeButton.classList.toggle('liked');
+
+      if (likeButton && likeButton.classList.contains('like-button')) {
+          likeButton.classList.toggle('liked');
       }
-    });
   });
-  
-  function createPostMarkup(post) {
-    return `
+});
+
+function createPostMarkup(post) {
+  return `
       <div class="post">
-        <img src="https://via.placeholder.com/50" alt="${post.username}" class="profile-image">
-        <div class="post-content">
-          <h4 class="mb-3">${post.username}</h4>
-          <p>${post.text}</p>
-          <div class="actions">
-            <div class="action-icons">
-              <span class="like-button"><i class="fas fa-heart"></i></span>
-              <i class="far fa-comment"></i>
-            </div>
-            <i class="far fa-bookmark"></i>
+          <img src="https://via.placeholder.com/50" alt="${post.username}" class="profile-image">
+          <div class="post-content">
+              <h4 class="mb-3">${post.username}</h4>
+              <p>${post.text}</p>
+              <div class="actions">
+                  <div class="action-icons">
+                      <span class="like-button"><i class="fas fa-heart"></i></span>
+                      <span><i class="far fa-comment"></i></span>
+                      <span><i class="far fa-bookmark"></i></span>
+                  </div>
+              </div>
           </div>
-        </div>
       </div>
-    `;
-  }
-  
-  function openNewPostForm() {
-    const thoughts = prompt('Condividi i tuoi pensieri:');
-    if (thoughts !== null && thoughts.trim() !== '') {
+  `;
+}
+
+function openNewPostForm() {
+  const thoughts = prompt('Condividi i tuoi pensieri:');
+  if (thoughts !== null && thoughts.trim() !== '') {
       const newPost = { username: 'Utente Nuovo', text: thoughts };
       posts.push(newPost);
-  
       renderPosts();
-    }
   }
-  
-  function renderPosts() {
-    const postsContainer = document.getElementById('posts-container');
-    postsContainer.innerHTML = '';
-  
-    posts.forEach(post => {
-      const postMarkup = createPostMarkup(post);
-      postsContainer.innerHTML += postMarkup;
-    });
-  }
-  
-  const posts = [
-    { username: 'utente1', text: 'Questo è un post di utente1' },
-    { username: 'utente2', text: 'Ecco cosa ha pubblicato utente2' },
-    // Aggiungi altri post secondo necessità
-  ];
-  
-  
+}
+
+function renderPosts() {
+  const postsContainer = document.getElementById('posts-container');
+  const fragment = document.createDocumentFragment();
+
+  posts.forEach(post => {
+      const postElement = document.createRange().createContextualFragment(createPostMarkup(post));
+      fragment.appendChild(postElement);
+  });
+
+  postsContainer.innerHTML = ''; // Pulisce il contenuto del container
+  postsContainer.appendChild(fragment); // Aggiunge il fragment al container
+}
+
+const posts = [
+  { username: 'utente1', text: 'Questo è un post di utente1' },
+  { username: 'utente2', text: 'Ecco cosa ha pubblicato utente2' },
+  // Aggiungi altri post secondo necessità
+];
