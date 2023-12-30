d5d2ee0c9a085e84d8f6c798cfba658161e04048
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-12-30 18:28:09 +0100

    inizio scrittura di index.css

--- src/index.php
+++ src/index.php
@@ -6,15 +6,16 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo $_GET['page']?></title>
-        <link rel="stylesheet" href="css/mainpage.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+        
         <script src="https://code.jquery.com/jquery-3.7.1.js"
                 integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
                 crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+        <link rel="stylesheet" href="css/index.css">
         
-    </head>
+
 
     <?php include "php/header.php"?>
 
