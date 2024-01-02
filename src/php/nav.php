2d3550de1758e67b082e6f9d75d63f7e58b46424
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-02 15:09:09 +0100

    formattazione del codice html per rispettare il pattern mvc

---
+++ src/php/nav.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="<?php echo $_SERVER['PHP_SELF']."?page=home"?>">timesCounter</a>
+        </div>
+        <ul class= "nav navbar-nav">
+            <li class="active">
+                <a href="<?php echo $_SERVER['PHP_SELF']."?page=home"?>">home</a>
+            </li>
+            <li>
+                <a href="<?php echo $_SERVER['PHP_SELF']."?page=about"?>">about</a>
+            </li>
+            <li>
+                <a href="<?php echo $_SERVER['PHP_SELF']."?page=login"?>">login</a>
+            </li>
+            <li>
+                <a href="<?php echo $_SERVER['PHP_SELF']."?page=mainpage"?>">main page</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
