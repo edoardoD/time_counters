0bcd7ecdaf4e7a9872b4ae55e0a2539d6a892585
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-02 15:43:01 +0100

    nav menu at top

--- src/index.php
+++ src/index.php
@@ -7,8 +7,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo $_GET['page']?></title>
         <!-- link bootstrap -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <!-- link css statico -->
         <link rel="stylesheet" href="css/index.css">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         
         
 
