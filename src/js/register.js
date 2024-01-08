e548404459dc917f6344749f534ce50f42001e3a
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-08 22:59:41 +0100

    funzioni di interazione come check dei temrini e ajac

--- src/js/register.js
+++ src/js/register.js
@@ -7,35 +7,43 @@ function gotoNewAccount() {
     document.getElementById("login-form").scrollIntoView();
 }
 
-// $('#signup').attr('disabled', 'disabled');
-// $('#agree-term').click(function() {
-//     if($(this).is(':checked')) {
-//         $('#signup').removeAttr('disabled');
-//     } else {
-//         $('#signup').attr('disabled', 'disabled');
-//     }
-// });
-
-
-
-$("#signup").click(function(){
-    alert($("#name").val());
-    $.ajax({
-        type:"POST",
-        url: "php/POST.php",
-        data: "request=register&name="+$("#name").val()+"&surname="+$("#surname").val()+"&tel="+$("#tel").val()+"&username="+$("#email").val()+"&password="+$("#password").val(),
-        dataType: "json",
-        success: function(data){
-            console.log(data);
-            if(data.result){
-                alert("registrazione effettuata");
-            }else{
-                alert("username già in uso"); 
-            }
-        },
-        error: function(data,status,error){
-            alert(data);
-            console.error(error);
+$(function(){
+    $('#signup').attr('disabled', 'disabled');
+    $('#agree-term').click(function() {
+        if($(this).is(':checked')) {
+            $('#signup').removeAttr('disabled');
+        } else {
+            $('#signup').attr('disabled', 'disabled');
         }
-    });
+    })
 });
+
+
+$(function(){
+    $("#signup").click(function(){
+        alert($("name").val());
+        $.ajax({
+            type:"POST",
+            url: "php/POST.php",
+            data: "request=register&name="+$("#name").val()+"&surname="+$("#surname").val()+"&tel="+$("#tel").val()+"&username="+$("#email").val()+"&password="+$("#password").val(),
+            dataType: "json",
+            success: function(data){
+                console.log(data);
+                if(data.result){
+                    alert("registrazione effettuata");
+                }else{
+                    alert("username già in uso"); 
+                }
+            },
+            error: function(data,status,error){
+                alert(data);
+                console.error(error);
+            }
+        });
+    })
+});
+
+
+
+
+
