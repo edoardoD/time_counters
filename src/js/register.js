a3d1dc474289fa3b1d7886c3c8d1f7131f1ccacb
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-08 22:28:22 +0100

    funzione ajax per registrazione

--- src/js/register.js
+++ src/js/register.js
@@ -7,42 +7,35 @@ function gotoNewAccount() {
     document.getElementById("login-form").scrollIntoView();
 }
 
-$('#signup').attr('disabled', 'disabled');
-$('#agree-term').click(function() {
-    if($(this).is(':checked')) {
-        $('#signup').removeAttr('disabled');
-    } else {
-        $('#signup').attr('disabled', 'disabled');
-    }
-});
+// $('#signup').attr('disabled', 'disabled');
+// $('#agree-term').click(function() {
+//     if($(this).is(':checked')) {
+//         $('#signup').removeAttr('disabled');
+//     } else {
+//         $('#signup').attr('disabled', 'disabled');
+//     }
+// });
 
-$.ajax({
-    type:"GET",
-    url: "php/GET.php",
-    data: "request=lingue",
-    dataType: "json",
-    success: function(data){
-        console.log("sto per mostrare le lingue");
-        console.log(data.lingue);
-        let html='';
-        //ordina data.lingue in modo che inglese sia il primo
-        data.lingue.sort(function(a,b){
-            if(a.nome=="inglese"){
-                return -1;
+
+
+$("#signup").click(function(){
+    alert($("#name").val());
+    $.ajax({
+        type:"POST",
+        url: "php/POST.php",
+        data: "request=register&name="+$("#name").val()+"&surname="+$("#surname").val()+"&tel="+$("#tel").val()+"&username="+$("#email").val()+"&password="+$("#password").val(),
+        dataType: "json",
+        success: function(data){
+            console.log(data);
+            if(data.result){
+                alert("registrazione effettuata");
             }else{
-                return 1;
+                alert("username già in uso"); 
             }
-        });
-        html +=`
-            <option selected disabled >lista delle lingue </option>`;
-        data.lingue.forEach(function(lingua){
-            html +=`
-                <option value="${lingua.nome}">${lingua.nome}</option>`;
-        })
-        $("#lingue").html(html);
-    },
-    error: function(data,status,error){
-        alert("errore del server!");
-        console.error(error);
-    }
+        },
+        error: function(data,status,error){
+            alert(data);
+            console.error(error);
+        }
+    });
 });
---
+++ src/php/POST.php
@@ -0,0 +1,81 @@
+<?php
+    require_once "jsonHeader.php";
+    require_once "globals.php";
+
+    $requests = [
+        'login' => login,
+        'register' => register
+    ];
+    if(isset($_POST['request'])){
+        if(array_key_exists($_POST['request'], $requests)){
+            $requests[$_POST['request']]();
+        }
+        else{
+            die(json_encode(["result"=>false, "error"=>"richiesta non valida"]));
+        }
+    }
+    else{
+        die(json_encode(["result"=>false, "error"=>"nessun parametro impostato"]));
+    }
+
+    function login() {
+        $conn = mysqli_connect($GLOBALS['host'],$GLOBALS['user'],$GLOBALS['password'],$GLOBALS['dbName']);
+        if (!$conn){
+            die(json_encode(["result"=>false,"error"=>"connessione non riuscita"]));
+        }
+        else{
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $query = "SELECT * FROM CLIENTI WHERE email = '$username' AND Password = '$password'";
+            $result= mysqli_query($conn,$query);
+            if($result){
+                $row= mysqli_fetch_assoc($result);
+                if($row){
+                    session_unset();
+                    session_start();
+                    $_SESSION['user'] = $row['email'];
+                    $_SESSION['lingua'] = $row['lingua'];
+                    die(json_encode(["result" => true, "hi" => "ben arrivayto ".$_SESSION['user']]));
+                }
+                else{
+                    die(json_encode("username o password errati"));
+                }
+            }
+            else{
+                die(json_encode("errore nella query"));
+            }
+        }
+        mysqli_close($conn);
+    }
+
+    function register() {
+        $conn = mysqli_connect($GLOBALS['host'],$GLOBALS['user'],$GLOBALS['password'],$GLOBALS['dbName']);
+        if (!$conn){
+            die(json_encode(["result"=>false,"error"=>"connessione non riuscita"]));
+        }
+        else{
+
+            $email = $_POST['username'];
+            $nome = $_POST['name'];
+            $cognome = $_POST['surname'];
+            $password = $_POST['password'];
+            $nazionalita = $_POST['nazionalita'];
+            $telefono = $_POST['tel'];
+            $lingua = $_POST['lingua'];    
+            
+            $query = "INSERT INTO CLIENTI (email, nome, cognome, password, nazionalita, telefono, lingua) 
+                    VALUES ('$email', '$nome', '$cognome', '$password', '$nazionalita', '$telefono','$lingua')";
+            
+
+            $result= mysqli_query($conn,$query);
+            if($result){
+                die(json_encode(["result" => true, "msg"=>"registrazione avvenuta con successo"]));
+            }
+            else{
+                $str = "errore nella query".$email." ".$nome." ".$cognome." ".$password." ".$nazionalita." ".$telefono." ".$lingua;
+                die(json_encode(["result"=>false, "error"=> $str]));
+            }
+        }
+        mysqli_close($conn);
+    }
+?>
