96e231ffe0065f96217095f443680132d90763d3
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-08 10:50:05 +0100

    upgrade font awensome reference

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <!-- https://unibowebprogramming.altervista.org/social/ -->
 <!-- HTML checker: http://achecker.csr.unibo.it/checker/index.php -->
 <!-- Validator: https://validator.w3.org/#validate_by_upload -->
+<!-- font awensome scripts: https://cdnjs.com/libraries/font-awesome -->
 
 <!DOCTYPE html>
 <html lang="it">
@@ -17,7 +18,10 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <!-- script di utility -->
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+        <!-- fot awensome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+            integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
         
         
         <?php require "php/paginator.php"?>
@@ -26,9 +30,9 @@
             <div class="container p-4 pb-0">
                 <div class="row">
                     <div class="col">
-                        <a href="<?php echo $_SERVER['PHP_SELF']."?page=about"?>" class="btn btn-outline-light btn-floating m-1"><i class="fas fa-info-circle"  id="info"></i></a>
-                        <a href="<?php echo $_SERVER['PHP_SELF']."?page=dashboard"?>" class="btn btn-outline-light btn-floating m-1"><i class="fas fa-user-circle " id="profile"></i></a>
-                        <a href="<?php echo $_SERVER['PHP_SELF']."?page=search"?>" class="btn btn-outline-light btn-floating m-1"><i class="fas fa-search" id="search"></i></a>
+                        <a href="<?php echo $_SERVER['PHP_SELF']."?page=about"?>" class="btn btn-outline-light btn-floating m-1 "><i class="fas fa-info-circle"  id="info"></i></a>
+                        <a href="<?php echo $_SERVER['PHP_SELF']."?page=dashboard"?>" class="btn btn-outline-light btn-floating m-1 "><i class="fas fa-user-circle " id="profile"></i></a>
+                        <a href="<?php echo $_SERVER['PHP_SELF']."?page=search"?>" class="btn btn-outline-light btn-floating m-1 "><i class="fas fa-search" id="search"></i></a>
                     </div>
                 </div>
             </div>
