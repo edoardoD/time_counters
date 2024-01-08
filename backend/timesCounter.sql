ea185aa8e30a025db79b0111da54e6b0038f3963
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-08 22:00:07 +0100

    fix database

--- backend/timesCounter.sql
+++ backend/timesCounter.sql
@@ -3,10 +3,18 @@
 -- *--------------------------------------------
 -- * DB-MAIN version: 11.0.2              
 -- * Generator date: Sep 14 2021              
--- * Generation date: Fri Jan  5 17:26:14 2024 
+-- * Generation date: Mon Jan  8 21:56:07 2024 
 -- * LUN file: C:\unibo\Webb\time_counters\backend\timesCounter.lun 
 -- * Schema: tramutazione schema logico/1-1-1 
 -- ********************************************* 
+
+
+-- Database Section
+-- ________________ 
+
+create database tramutazione schema logico;
+use tramutazione schema logico;
+
 
 -- Tables Section
 -- _____________ 
@@ -47,8 +55,8 @@ create table MARCHI (
      anno_nascita  int not null,
      constraint IDMARCHIO primary key (nome))ENGINE=InnoDB;
 
-create table OROLOGII (
-     referenza char(20) not null,
+create table OROLOGI (
+     referenza char(15) not null,
      sesso char(1) not null,
      dim_cassa int not null,
      spessore int not null,
@@ -62,20 +70,20 @@ create table POST (
      utente varchar(40) not null,
      constraint IDPOST_ID primary key (id_post))ENGINE=InnoDB;
 
-create table PUBBLICAZIONE (
+create table PUBBLICAZIONI (
      referenza char(20) not null,
      id_post int not null,
      constraint IDPUBBLICAZIONE primary key (id_post, referenza))ENGINE=InnoDB;
 
-create table SEGUITO (
+create table SEGUITI (
      utente2 varchar(40) not null,
      utente1 varchar(40) not null,
      constraint IDSEGUITO primary key (utente2, utente1))ENGINE=InnoDB;
 
-create table UTENTE (
+create table UTENTI (
      email varchar(40) not null,
      nome char(40) not null,
-     cognome char(40) not null,
+     cognome char(20) not null,
      password varchar(20) not null,
      constraint IDUTENTE primary key (email))ENGINE=InnoDB;
 
@@ -84,80 +92,80 @@ create table UTENTE (
 -- ___________________ 
 
 -- Not implemented
--- alter table CATEGORIA add constraint IDCATEGORIA_CHK
+-- alter table CATEGORIE add constraint IDCATEGORIA_CHK
 --     check(exists(select * from COMBINAZIONI
 --                  where COMBINAZIONI.categoria = nome)); 
 
 alter table COMBINAZIONI add constraint FKINC_LIN
      foreign key (linea)
-     references LINEA (nome);
+     references LINEE (nome);
 
 alter table COMBINAZIONI add constraint FKINC_CAT
      foreign key (categoria)
-     references CATEGORIA (nome);
+     references CATEGORIE (nome);
 
-alter table COMMENTO add constraint FKSCRTTURA
+alter table COMMENTI add constraint FKSCRTTURA
      foreign key (utente)
-     references UTENTE (email);
+     references UTENTI (email);
 
-alter table COMMENTO add constraint FKOPINIONE
+alter table COMMENTI add constraint FKOPINIONE
      foreign key (id_post)
      references POST (id_post);
 
-alter table CONVERSAZIONE add constraint FKCOMMENTA
+alter table CONVERSAZIONI add constraint FKCOMMENTA
      foreign key (id_commento)
-     references COMMENTO (id);
+     references COMMENTI (id);
 
-alter table CONVERSAZIONE add constraint FKRISPOSTA
+alter table CONVERSAZIONI add constraint FKRISPOSTA
      foreign key (id_risposta)
-     references COMMENTO (id);
+     references COMMENTI (id);
 
-alter table IMMAGINE add constraint FKCONTENIMENTO
+alter table IMMAGINI add constraint FKCONTENIMENTO
      foreign key (id_post)
      references POST (id_post);
 
 -- Not implemented
--- alter table LINEA add constraint IDLINEA_CHK
---     check(exists(select * from OROLOGIO
---                  where OROLOGIO.linea = nome)); 
+-- alter table LINEE add constraint IDLINEA_CHK
+--     check(exists(select * from OROLOGI
+--                  where OROLOGI.linea = nome)); 
 
-alter table LINEA add constraint FKDESING
+alter table LINEE add constraint FKDESING
      foreign key (marchio)
-     references MARCHIO (nome);
+     references MARCHI (nome);
 
-alter table OROLOGIO add constraint FKPRODUZIONE
+alter table OROLOGI add constraint FKPRODUZIONE
      foreign key (linea)
-     references LINEA (nome);
+     references LINEE (nome);
 
 -- Not implemented
 -- alter table POST add constraint IDPOST_CHK
---     check(exists(select * from IMMAGINE
---                  where IMMAGINE.id_post = id_post)); 
+--     check(exists(select * from IMMAGINI
+--                  where IMMAGINI.id_post = id_post)); 
 
 -- Not implemented
 -- alter table POST add constraint IDPOST_CHK
---     check(exists(select * from COMMENTO
---                  where COMMENTO.id_post = id_post)); 
+--     check(exists(select * from COMMENTI
+--                  where COMMENTI.id_post = id_post)); 
 
 alter table POST add constraint FKCREA
      foreign key (utente)
-     references UTENTE (email);
+     references UTENTI (email);
 
-alter table PUBBLICAZIONE add constraint FKPUB_POS
+alter table PUBBLICAZIONI add constraint FKPUB_POS
      foreign key (id_post)
      references POST (id_post);
 
-alter table PUBBLICAZIONE add constraint FKPUB_ORO
+alter table PUBBLICAZIONI add constraint FKPUB_ORO
      foreign key (referenza)
-     references OROLOGIO (referenza);
+     references OROLOGI (referenza);
 
-alter table SEGUITO add constraint FKSEGUITO
+alter table SEGUITI add constraint FKSEGUITO
      foreign key (utente1)
-     references UTENTE (email);
+     references UTENTI (email);
 
-alter table SEGUITO add constraint FKSEGUACE
+alter table SEGUITI add constraint FKSEGUACE
      foreign key (utente2)
-     references UTENTE (email);
+     references UTENTI (email);
 
 
 -- Index Section
--- src/js/register.js
+++ src/js/register.js
@@ -2,3 +2,47 @@
 function scrolToLog() {
     document.getElementById("login").scrollIntoView();
 }
+
+function gotoNewAccount() {
+    document.getElementById("login-form").scrollIntoView();
+}
+
+$('#signup').attr('disabled', 'disabled');
+$('#agree-term').click(function() {
+    if($(this).is(':checked')) {
+        $('#signup').removeAttr('disabled');
+    } else {
+        $('#signup').attr('disabled', 'disabled');
+    }
+});
+
+$.ajax({
+    type:"GET",
+    url: "php/GET.php",
+    data: "request=lingue",
+    dataType: "json",
+    success: function(data){
+        console.log("sto per mostrare le lingue");
+        console.log(data.lingue);
+        let html='';
+        //ordina data.lingue in modo che inglese sia il primo
+        data.lingue.sort(function(a,b){
+            if(a.nome=="inglese"){
+                return -1;
+            }else{
+                return 1;
+            }
+        });
+        html +=`
+            <option selected disabled >lista delle lingue </option>`;
+        data.lingue.forEach(function(lingua){
+            html +=`
+                <option value="${lingua.nome}">${lingua.nome}</option>`;
+        })
+        $("#lingue").html(html);
+    },
+    error: function(data,status,error){
+        alert("errore del server!");
+        console.error(error);
+    }
+});
