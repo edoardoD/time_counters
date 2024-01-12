719d100373b61896908158f8c5259caa5238ee5b
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-12 22:51:01 +0100

    making router for requests

--- src/php/router.php
+++ src/php/router.php
@@ -1,4 +1,39 @@
 <?php
+require_once "jsonHeader.php";
 
+$get_requests = [];
+
+$post_requests = [
+    'login' => 'post/login.php',
+    'register' => 'post/register.php'
+];
+
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    if(isset($_GET['request'])){
+        if(array_key_exists($_GET['request'], $get_requests)){
+            require_once $get_requests[$_GET['request']];
+        }
+        else{
+            die(json_encode(["result"=>false, "error"=>"richiesta non valida"]));
+        }
+    }
+    else{
+        die(json_encode(["result"=>false, "error"=>"nessun parametro impostato"]));
+    }
+}
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if(isset($_POST['request'])){
+        if(array_key_exists($_POST['request'], $post_requests)){
+            require_once $post_requests[$_POST['request']];
+        }
+        else{
+            die(json_encode(["result"=>false, "error"=>"richiesta non valida"]));
+        }
+    }
+    else{
+        die(json_encode(["result"=>false, "error"=>"nessun parametro impostato"]));
+    }
+}
 
 ?>
