fa82469306d0ff53f78f1df5de7ba68ddc022af6
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-12 16:15:01 +0100

    add sweetalert message an

--- src/js/register.js
+++ src/js/register.js
@@ -1,3 +1,39 @@
+// https://code.tutsplus.com/creating-pretty-popup-messages-using-sweetalert2--cms-30662t
+
+
+/*
+
+
+document.querySelector('.swal2-popup-custom').style.marginTop = marginTop + 'px';
+
+*/
+
+$(function () {
+
+    navbarHeight = document.querySelector('#nav-menu').offsetHeight;
+    var marginTop = navbarHeight + 20; // Aggiungi 20px al margine
+    toastMixin = Swal.mixin({
+        toast: true,
+        icon: 'success',
+        title: 'General Title',
+        animation: false,
+        position: 'top-right',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+          document.querySelector('.swal2-popup-custom').style.marginTop = marginTop + 'px';
+        },
+        customClass:{
+            popup:'swal2-popup-custom'
+        }
+    });
+
+});
+
+
 
 function scrolToLog() {
     document.getElementById("login").scrollIntoView();
@@ -7,12 +43,10 @@ function gotoNewAccount() {
     document.getElementById("login-form").scrollIntoView();
 }
 
-// $(function () {
-//     $("#signup").click(function () {
-//         alert($("#name").val());
 
-//     })
-// });
+
+
+
 
 // seleziono gli elementi del form
 //document.querySelector("register-form")=> cerca un elemento <register-form>
@@ -20,33 +54,20 @@ $(function () {
     var pass = document.getElementById("pass");
     form = document.querySelector("#register-form");
     var re_pass = document.getElementById("re_pass");
-    var error = document.createElement("div"); // creo un elemento per mostrare l'errore
 
-    // aggiungo uno stile all'elemento di errore
-    error.style.color = "red";
-    error.style.fontWeight = "bold";
-    error.style.display = "none"; // nascondo l'elemento finché non serve
-
-    // aggiungo l'elemento di errore al form
-    form.appendChild(error);
     let form_tag = "#"+form.id;
     // aggiungo un evento al submit del form d
     $(form_tag).submit(function (event) {
         event.preventDefault(); // previene l'invio del form
         if (pass.value === re_pass.value) {
-            console.log("Valori da inviare:\n" +
-                "Nome: " + $("#name").val() + "\n" +
-                "Cognome: " + $("#surname").val() + "\n" +
-                "Username: " + $("#email").val() + "\n" +
-                "Password: " + $("#pass").val());
             registerRequest($("#name").val(),$("#surname").val(),$("#email").val(),$("#pass").val());
         } else {
-            // se non sono uguali, mostro l'errore
-            // error.textContent = "";
-            // error.style.display = "block";
-            alert("Le password non coincidono. Per favore, correggile.");
+            
+            toastMixin.fire({
+                title: 'password not matching',
+                icon: 'error'
+            });
         }
-
     });
 });
 
@@ -59,13 +80,22 @@ function registerRequest(name,surname,email,pass) {
         success: function (data) {
             console.log(data);
             if (data.result) {
-                alert("registrazione effettuata");
+                toastMixin.fire({
+                    animation: true,
+                    title: 'Signed in Successfully'
+                });
             } else {
-                alert("username già in uso");
+                toastMixin.fire({
+                    title: 'username already used',
+                    icon:'error'
+                });
             }
         },
         error: function (data, status, error) {
-            alert("errore del server" + error);
+            toastMixin.fire({
+                title: 'il server non risponde',
+                icon:'error'
+            });
             console.log(data);
             console.log(status);
         }
