296d731da916d10ccaae7d0ddf2df310fd8b4ac3
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-13 15:52:30 +0100

    fixed register for match client data and respond with json

--- src/php/post/register.php
+++ src/php/post/register.php
@@ -1,37 +1,29 @@
 <?php
 
-require "../global.php";
 
-$conn = new mysqli($host, $utente, $password, $database);
+$conn = new mysqli($GLOBALS['host'], $GLOBALS['utente'], $GLOBALS['password'], $GLOBALS['database']);
 
 // Verifica della connessione
 if ($conn->connect_error) {
     die(json_encode(["result"=>false,"error"=>"connessione non riuscita". $conn->connect_error]));
 }
 
-// Processa il form quando viene inviato
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $surname = $_POST["surname"];
-    $password = $_POST["re_pass"];
-    $email = $_POST["email"];
+$name = $_POST["name"];
+$surname = $_POST["surname"];
+$email = $_POST["email"];
+$password = $_POST["pass"];
 
-    // Puoi aggiungere ulteriori controlli di validazione qui
-    if (empty($password)) {
-        die(json_encode(["result"=>false,"error"=>"connessione non riuscita". $conn->connect_error]));
-    }
-
-    // Utilizzo di istruzioni preparate per evitare SQL injection
-    $stmt = $conn->prepare("INSERT INTO UTENTI (email, nome, cognome, password) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss", $email, $name, $surname, $password);
-
-    if ($stmt->execute()) {
-        die(json_encode(["result"=>true,"result"=>"Utente registrato con successo"]));
-    }
-
-    // Chiudi la dichiarazione preparata
-    $stmt->close();
+if (empty($password)) {
+    die(json_encode(["result"=>false,"error"=>"la password non può essere vuota". $conn->connect_error]));
 }
+// Utilizzo di istruzioni preparate per evitare SQL injection
+$stmt = $conn->prepare("INSERT INTO UTENTI (email, nome, cognome, password) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $email, $name, $surname, $password);
+if ($stmt->execute()) {
+    die(json_encode(["result"=>true,"result"=>"Utente registrato con successo"]));
+}
+// Chiudi la dichiarazione preparata
+$stmt->close();
 
 // Chiudi la connessione al database
 $conn->close();
