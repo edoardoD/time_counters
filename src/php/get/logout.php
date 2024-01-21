96618bbef721cf1f05a77cffd1c73bceba5721b6
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-21 12:04:06 +0100

    logout fixed

--- src/php/get/logout.php
+++ src/php/get/logout.php
@@ -1,20 +1,8 @@
 <?php
-<<<<<<< HEAD
-
-    session_destroy();
-
-    // Check if session is destroyed
-    if (!isset($_SESSION)) {
-        die(json_encode(["result"=>true, "messagge"=>"Utente disconnesso correttamente"]));
-    } else {
-        die(json_encode(["result"=>false, "error"=>"Utente non disconnesso"]));
-    }
-=======
     $_SESSION = array();
     if(session_destroy()){
         die(json_encode(["result"=>true,"message"=>"logout riuscito"]));
     }
     die(json_encode(["result"=>false,"error"=>"logout non riuscito"]));
->>>>>>> 93268eea6bde1b0d3e1a0a3ac77276ceb2abbd45
     
 ?>
