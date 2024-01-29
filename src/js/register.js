9bb840c8d2c660cc0b6aec68db16ee56689e3dad
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-29 23:27:05 +0100

    fixed message toast

--- src/js/register.js
+++ src/js/register.js
@@ -75,7 +75,7 @@ function registerRequest(name, surname, email, pass) {
                 if (data.result) {
                     window.generalToast.fire({
                         animation: true,
-                        title: data.messagge,
+                        title: data.message,
                         didOpen: (toast) => {
                             document.querySelector('.swal2-popup-custom').style.marginTop = marginTop + 'px';
                         }
