fd41f53a57dd89aa8697d33722eafa6801f719d3
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-29 23:35:19 +0100

    ridotto il tempo di visualizzazione del toast

--- src/js/index.js
+++ src/js/index.js
@@ -51,7 +51,7 @@ $(function () {
         animation: false,
         position: 'top-right',
         showConfirmButton: false,
-        timer: 3000,
+        timer: 2000,
         timerProgressBar: true,
         didOpen: (toast) => {
           toast.addEventListener('mouseenter', Swal.stopTimer)
