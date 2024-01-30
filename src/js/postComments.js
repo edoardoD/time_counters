4eea6795f7ec81936f6455e61cbfc3e24fb70f30
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-01-30 15:05:33 +0100

    first post comment

---
+++ src/js/postComments.js
@@ -0,0 +1,17 @@
+let html = `
+
+<div class="bg-red">
+        <div class="d-flex flex-column bg-opacity-10 bg-dark mx-2 px-3 " style="border-radius: 18px;">
+          <div class="d-flex flex-column m-1">
+            <span class="m-0 p-0 text-dark fw-bold fs-7" type="button">Mark Z.</span>
+            <span class="m-0 p-0 text-dark ">Tha's Great. Keep it up.</span>
+          </div>
+        </div>
+        <div class="mx-2 p-0 d-flex justify-content-start fs-7 text-muted ">
+          <div class="mx-2 fw-bold" type="button">Like</div>
+          <div class="mx-2 fw-bold" type="button">Reply</div>
+          <div class="mx-2 fw-bold" type="button">Share</div>
+          <div class="mx-2" type="button">1d</div>
+        </div>
+      </div>
+</div>`;
