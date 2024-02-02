3d82daec66710ab8dc2ec7fe26cca4a7a80ce611
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-02-02 22:16:13 +0100

    event on collapsing

--- src/js/mainpage.js
+++ src/js/mainpage.js
@@ -13,9 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 {/* <img src="${post.profileImage}" alt="${post.username}" class="profile-image"></img> */ }
 function createPostMarkup(post) {
-  let commentId = ("" + post.username + post.id).replace(/\s/g, '');
-
-
+  let postKey = post.username+"_"+post.id; 
+  let commentKey = post.username+""+post.id; 
+  console.log(postKey);
   return `
       <div class="post">
         <img src="${post.profileImage}" alt="${post.nome}" class="profile-image">
@@ -30,7 +30,7 @@ function createPostMarkup(post) {
                       id="faceCollapse"
                       class=" collapsed"
                       data-bs-toggle="collapse"
-                      href="#${commentId}"
+                      href="#${commentKey}"
                       role="button"
                       aria-expanded="false"
                       aria-controls="collapseExample"
@@ -39,15 +39,15 @@ function createPostMarkup(post) {
                 </div>
             </div>
         </div>
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="inserisci un commento" aria-label="Input group example" aria-describedby="basic-addon1">
-                <span class="input-group-text" id="basic-addon1" style="cursor:pointer;">
+        <div id="${postKey}" class="input-group">
+          <input type="text" name="textComment" class="form-control" placeholder="inserisci un commento" aria-label="Input group example" aria-describedby="basic-addon1">
+                <span class="input-group-text" onclick="chargeComment(this.parentElement.id)" id="basic-addon1" style="cursor:pointer;">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
-          <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
-          </svg>
+                    <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
+                  </svg>
                 </span>
         </div>
-        <div id="${commentId}" class="collapse mt-3 scrollable">
+        <div id="${commentKey}" class="collapse mt-3 scrollable">
         </div>
       </div>
   `;
@@ -174,6 +174,11 @@ function renderPosts(posts) {
   posts.forEach(post => {
     const postElement = document.createRange().createContextualFragment(createPostMarkup(post));
     fragment.appendChild(postElement);
+    
+    const myCollapsible = fragment.getElementById(post.username+""+post.id)
+      myCollapsible.addEventListener('show.bs.collapse', event => {
+      console.log('collapsing');
+    })
   });
 
   postsContainer.innerHTML = ''; // Pulisce il contenuto del container
