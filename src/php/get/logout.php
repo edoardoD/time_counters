a193af12c773f75304d477a32fcd8d73bc00b145
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-02-07 23:43:36 +0100

    clear

--- src/php/get/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $_SESSION = array();
-    if(session_destroy()){
-        die(json_encode(["result"=>true,"message"=>"logout riuscito"]));
-    }
-    die(json_encode(["result"=>false,"error"=>"logout non riuscito"]));
-    
-?>
--- src/php/global.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Connessione al database (sostituisci con i tuoi dati)
-    $host = "localhost"; // Può variare in base al tuo server
-    $utente = "root";
-    $database = "my_unibowebprogramming";
-    $password = "";
-?>
